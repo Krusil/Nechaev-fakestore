be3d89d7f7d1404dbf6ff8dfcc3d4ad592a7aae9
Krusil <nik.nechaev03@mail.ru>
2023-06-01 22:24:50 +0500

    second commit

---
+++ Nechaev-Store/src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <header>
+      <img className="header__logo" src="https://img.freepik.com/premium-vector/shopping-bag-store-logo-online-shopping-logo-design_487414-262.jpg?w=740" alt="" />
+      <h1>Nechaev Store</h1>
+    </header>
+  );
+};
+
+export default Header;
